9251cb3301d143d209541cb0edf46df310dbab28
kolako54 <parhamwolf051@gmail.com>
2020-04-26 20:17:43 +0430

    Updates

---
+++ precache-manifest.96a0565283ebbb3cdf179cb0a6100f65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f44e58765201c46855eef0f6bc327df",
+    "url": "/login-page4/index.html"
+  },
+  {
+    "revision": "6c2f0032f964e964e7e1",
+    "url": "/login-page4/static/css/main.c0f1bef6.chunk.css"
+  },
+  {
+    "revision": "1e724dee6acd5dba9826",
+    "url": "/login-page4/static/js/2.b3e37497.chunk.js"
+  },
+  {
+    "revision": "6c2f0032f964e964e7e1",
+    "url": "/login-page4/static/js/main.fb54bf58.chunk.js"
+  },
+  {
+    "revision": "4bd010b75a3cfbcc06f1",
+    "url": "/login-page4/static/js/runtime-main.27ee229e.js"
+  },
+  {
+    "revision": "358cf17eb92a8747b28e0a0e9b797317",
+    "url": "/login-page4/static/media/Logo.358cf17e.svg"
+  },
+  {
+    "revision": "f2bd4a28e444d8f2ac28e9ce07b486b2",
+    "url": "/login-page4/static/media/Vazir-Medium.f2bd4a28.woff"
+  },
+  {
+    "revision": "80b42e97beac2535b0b3acee1d1edaec",
+    "url": "/login-page4/static/media/aiside.80b42e97.svg"
+  }
+]);
